79daaa84a3bd0e41c554f8497b91beca81abd0b0
Ariel Videla <ariel_videla@hotmail.com>
2024-03-08 19:16:53 -0300

    third commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,12 +8,11 @@ import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 
-
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { duration: 3000 });
-  const isInView = useInView(ref, {once: true});
+  const isInView = useInView(ref, { once: true });
 
   useEffect(() => {
     if (isInView) {
@@ -23,11 +22,11 @@ const AnimatedNumbers = ({ value }) => {
 
   useEffect(() => {
     springValue.on("change", (latest) => {
-      if(ref.current && latest.toFixed(0) <= value ) {
+      if (ref.current && latest.toFixed(0) <= value) {
         ref.current.textContent = latest.toFixed(0);
       }
-    })
-  }, [springValue, value])
+    });
+  }, [springValue, value]);
 
   return <span ref={ref}></span>;
 };
@@ -41,17 +40,20 @@ const about = () => {
       </Head>
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="FullStack Developer" className="mb-16" />
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start">
+          <AnimatedText
+            text="FullStack Developer"
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Perfil
               </h2>
               <p className="my-4 font-medium">
-                Soy Lic. en Comercio Internacional, hace mas de dos años descubrí el
-                mundo de la programación y busco experiencia laboral en el rubro
-                para seguir aprendiendo. Actualmente soy desarrolador Jr. Full
-                Stack.
+                Soy Lic. en Comercio Internacional, hace mas de dos años
+                descubrí el mundo de la programación y busco experiencia laboral
+                en el rubro para seguir aprendiendo. Actualmente soy
+                desarrolador Jr. Full Stack.
               </p>
               <p className="font-medium">
                 Tengo conocimientos en HTML, CSS, SASS, JAVA SCRIPT, REACT JS,
@@ -70,7 +72,7 @@ const about = () => {
                 intuitivas y agradables para los usuarios.
               </p>
             </div>
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
@@ -81,32 +83,38 @@ const about = () => {
                 (max-width: 1200px) 50vw, 33vw"
               />
             </div>
-            <div className="col-span-2 flex flex-col items-end justify-between">
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={20} />+</span>
-                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75">
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center  md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text4xl">
+                  <AnimatedNumbers value={20} />+
+                </span>
+                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Proyectos completos
                 </h2>
               </div>
               <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={3} />+</span>
-                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text4xl">
+                  <AnimatedNumbers value={3} />+
+                </span>
+                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Años de experiencia
                 </h2>
               </div>
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={15} />+</span>
-                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text4xl">
+                  <AnimatedNumbers value={15} />+
+                </span>
+                <h2 className="text-xl font-medium text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Proyectos en proceso
                 </h2>
               </div>
             </div>
           </div>
           <Skills />
-          <div className="grid w-full grid-cols-8 gap-16 my-20">
+          <div className="grid w-full grid-cols-8 gap-16 my-20 sm:gap-8">
             <div className="col-span-12 flex flex-col items-start justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
-              Experiencia laboral
+                Experiencia laboral
               </h2>
               <Experience />
             </div>
@@ -118,4 +126,3 @@ const about = () => {
 };
 
 export default about;
-//4:59:23
