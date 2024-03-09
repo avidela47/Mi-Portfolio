b284f02907810ead2b39303bca19972f55bbbc55
Ariel Videla <ariel_videla@hotmail.com>
2024-03-09 15:24:53 -0300

    fourth commit

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -35,7 +35,7 @@ const Experience = () => {
   });
   return (
     <div className="my-34">
-      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full xs:w-full">
         <motion.div
           style={{ scaleY: scrollYProgress }}
           className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[0px]"
