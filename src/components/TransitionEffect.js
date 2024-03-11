570e0882e1abed53d9161c7d456b2c61bc6ae494
Ariel Videla <ariel_videla@hotmail.com>
2024-03-11 11:20:49 -0300

    sixth commit

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,20 +5,20 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-lila"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-gradient-to-r from-violet-500 to-fuchsia-500"
         initial={{ x:"100%", width:"100%" }}
         animate={{ x:"0%", width:"0%" }}
         exit={{ x:["0%", "100%"], width:["0%", "100%"] }}
         transition={{ duration:0.8, ease:"easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-lila2"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-gradient-to-r from-violet-500 to-fuchsia-500"
         initial={{ x:"100%", width:"100%" }}
         animate={{ x:"0%", width:"0%" }}
         transition={{ delay:0.2, duration:0.8, ease:"easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-gris"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-gradient-to-r from-violet-500 to-fuchsia-500"
         initial={{ x:"100%", width:"100%" }}
         animate={{ x:"0%", width:"0%" }}
         transition={{ delay:0.4, duration:0.8, ease:"easeInOut" }}
